3587fb20ce48fb70b96a5c87998b412b0ee24cc1
Yavor <y.trassiev@gmail.com>
2014-12-08 14:15:29 +0100

    Fixed a bug in my last commit

--- navigator/src/navigator.cpp
+++ navigator/src/navigator.cpp
@@ -152,13 +152,12 @@ navigator::navigator(int argc, char *argv[]){
     ROSUtil::getParam(n, "/topic_list/controller_topics/motor3/subscribed/twist_topic", motor3_pub_topic);
     std::string ir_pub_topic;
     ROSUtil::getParam(n, "/topic_list/hardware_topics/ir_sensors/published/ir_distance_topic", ir_pub_topic);
-    sub_sensor = n.subscribe("/ir_sensors/dists", 1000, &navigator::sensorCallback, this); 
-    
+     
+    sub_map = n.subscribe("/topological_map/map", 1000, &navigator::topologicalCallback, this);
     
     
     pub_motor = n.advertise<geometry_msgs::Twist>( motor3_pub_topic, 1000);
     sub_sensor = n.subscribe(ir_pub_topic, 1000, &navigator::sensorCallback, this);
-    sub_map = n.subscribe("/map/map", 1000, &navigator::topologicalCallback, this);
     sub_odometry = n.subscribe(odometry_pub_topic, 1000, &navigator::odometryCallback, this);
     //sub_pose = n.subscribe("/odometry/pose", 1000, &navigator::poseCallback, this);
 
